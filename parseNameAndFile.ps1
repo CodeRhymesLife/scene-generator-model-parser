0a4eb8b10423d35132de939b1cd55b99e200257f
Ryan James <rcjames1004@gmail.com>
2015-11-15 10:38:12 -0800

    Parses json file and adds names to model files

---
+++ parseNameAndFile.ps1
@@ -0,0 +1,15 @@
+﻿param([string] $Folder)
+
+# Parse the json
+$nameAndFile = Get-Content -Raw -Path $Folder/nameAndFile.json | ConvertFrom-Json
+# ConvertTo-Json $nameAndFile | Write-Output
+
+# Create a new folder for our model files
+$namedModelsFolder = "namedModels"
+New-Item $Folder/$namedModelsFolder -ItemType directory -Force
+
+$nameAndFile | ForEach {
+    $sourcePath = "$Folder/" + $_.file
+    $targetPath = "$Folder/$namedModelsFolder/" + $_.name + ".obj"
+    Copy-Item $sourcePath $targetPath
+}
