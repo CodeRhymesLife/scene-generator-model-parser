d1c57ea22d8d3ebe77e203b210ca0a964e9d5828
Ryan James <rcjames1004@gmail.com>
2015-12-21 16:30:57 -0800

    Add switches for renaming files and commands

--- run.ps1
+++ run.ps1
@@ -1,3 +1,12 @@
+Param(
+  [switch]$Rename,
+  [switch]$ShowCommands
+)
+
+if($ShowCommands) {
+    Set-PSDebug -Trace 1
+}
+
 Function Select-SceneFolder() {
 
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null     
@@ -23,13 +32,20 @@ if([string]::IsNullOrEmpty($sceneFolder)) {
 
 $modelName = Read-Host "What does this scene model? (e.g. Brain, Heart, ect)"
 
-# Add the correct name to each model file
-. ((Split-Path $MyInvocation.MyCommand.Path) + "\parseNameAndFile.ps1")
-$namedModelFolder = "$sceneFolder\models\namedModels"
-AddNamesToModels -Folder $sceneFolder -NamedModelFolder $namedModelFolder
+$modelsFolder = "$sceneFolder\models"
+if($Rename) {
+    # Add the correct name to each model file
+    . ((Split-Path $MyInvocation.MyCommand.Path) + "\parseNameAndFile.ps1")
+    $modelsFolder = "$sceneFolder\models\namedModels"
+    AddNamesToModels -Folder $sceneFolder -NamedModelFolder $modelsFolder
+}
 
 # Start Blender in the background
 # And run the script that loads all objects and places them at origin
-Start-Process -FilePath "C:\Program Files\Blender Foundation\Blender\blender.exe" -ArgumentList "--background --python centerObjectsInBlender.py -- --folder `"$namedModelFolder`" --newModelName `"$modelName`""
+Start-Process -FilePath "C:\Program Files\Blender Foundation\Blender\blender.exe" -ArgumentList "--background --python centerObjectsInBlender.py -- --folder `"$modelsFolder`" --newModelName `"$modelName`""
 
-Start $namedModelFolder
+Start $modelsFolder
+
+if($ShowCommands) {
+    Set-PSDebug -Trace 0
+}
