e24f9de5676f68abf58d3eaf182f4ce604e28295
rcjames1004 <rcjames1004@gmail.com>
2015-12-31 13:33:10 -0800

    Create a json file with organ part offsets
    In order to load meshes in Unity we need to know about their
    offsets--both the organ's offset in the whole body, and each organ
    part's offset within an organ, We need to know this information to
    center each object at the organ when necessary, and to position the
    organ in it's parent when necessary.

--- centerObjectsInBlender.py
+++ centerObjectsInBlender.py
@@ -18,16 +18,14 @@ completeModelPath = folder + "\_complete"
 if not os.path.exists(completeModelPath):
     os.makedirs(completeModelPath)
 
-import bpy		
+def location_to_json (location):
+    return {
+        "x": location.x,
+        "y": location.y,
+        "z": location.z,
+    }
 
-def get_object_global_location(obj):
-	v = obj.data.vertices[0].co
-	mat = obj.matrix_world
-
-	# Multiply matrix by vertex
-	loc = mat * v
-	print(loc)
-	return loc
+import bpy
 
 # Remove all objects from the scene
 # NOTE:
@@ -63,43 +61,62 @@ for item in obj_list:
 # Select all objects then center the entire model
 
 # gather list of items of interest and select only them
-candidate_list = [item.name for item in bpy.data.objects if item.type == "MESH"]
-for object_name in candidate_list:
+organ_parts = [item.name for item in bpy.data.objects if item.type == "MESH"]
+for object_name in organ_parts:
 	bpy.data.objects[object_name].select = True
 
-# Use the last object in the list to calculate the models relative position in the body
-objectUsedToCalculateRelativeModelPosition = bpy.data.objects[candidate_list[-1]]
-objectUsedToCalculateRelativeModelPosition_oldLocation = get_object_global_location(objectUsedToCalculateRelativeModelPosition)
+# Get the 3D view context so we can begin moving things around
+context = None
+for area in bpy.context.screen.areas:
+	if area.type == 'VIEW_3D':
+		context = bpy.context.copy()
+		context['area'] = area
+		break;
+
+# Store metadata about this organ
+organ_metadata = {};
+metadata_filename = completeModelPath + "\\" + "organ_metadata.json"
 
 # Move the origin to the center of the object
 bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
 
+# Move the 3D cursor to the center of the geometry and record it's position
+# This is the organ's relative position in the body
+bpy.ops.view3d.snap_cursor_to_selected(context)
+organ_metadata["bodyOffset"] = location_to_json(bpy.context.scene.cursor_location)
+
 # Center the whole model by:
-#	1) Snap the cursor to the center
+#	1) Snap the 3D cursor to the center
 #	2) Snap the selected objects to the cursor
-for area in bpy.context.screen.areas:
-	if area.type == 'VIEW_3D':
-		override = bpy.context.copy()
-		override['area'] = area
-		bpy.ops.view3d.snap_cursor_to_center(override)
-		bpy.ops.view3d.snap_selected_to_cursor(override, use_offset=True)
-		break
+bpy.ops.view3d.snap_cursor_to_center(context)
+bpy.ops.view3d.snap_selected_to_cursor(context, use_offset=True)
 
-# Now that the model have moved calculate its old origin by measuring the displacement of the measuring object
-objectUsedToCalculateRelativeModelPosition_newLocation = get_object_global_location(objectUsedToCalculateRelativeModelPosition)
-relativeModelPosition = objectUsedToCalculateRelativeModelPosition_oldLocation - objectUsedToCalculateRelativeModelPosition_newLocation
-
+# Save whole model
 completeModelFileName = completeModelPath + "\\" + completeObjName + ".obj"
 bpy.ops.export_scene.obj(filepath=completeModelFileName, use_selection=True)
 
-# Add the original position to the .obj file as a comment
-file = open(completeModelFileName,'r')
-data = file.read()
-file.close()
+# Save the offset of individual parts
+organ_part_offsets = organ_metadata["parts"] = {};
 
-file = open(completeModelFileName, 'w')
-positionStr = ', '.join(str(val) for val in relativeModelPosition)
-file.write("# relative position: [" + positionStr + "]\n" + data)
+# Loop over each part
+#   1) Save it's offset within the organ
+#   2) Deselect it
+for object_name in organ_parts:
+    organ_part = bpy.data.objects[object_name]
+    organ_part_offsets[object_name] = location_to_json(organ_part.location)
+    organ_part.select = False
 
-file.write(data)
-file.close()
+# Save the object with it's origin at the center
+for object_name in organ_parts:
+    organ_part = bpy.data.objects[object_name]
+    organ_part.select = True
+    bpy.ops.view3d.snap_cursor_to_center(context)
+    bpy.ops.view3d.snap_selected_to_cursor(context, use_offset=False)
+    organ_part_filename = completeModelPath + "\\" + object_name + ".obj"
+    bpy.ops.export_scene.obj(filepath=organ_part_filename, use_selection=True)
+    organ_part.select = False
+
+# Save the organ metadata to a json file
+import json
+with open(metadata_filename, 'w') as outfile:
+    json.dump(organ_metadata, outfile)
